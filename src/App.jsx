f39852392261cfd36de47e742a24234590410d9c
Chizaram <chukwumaobidivine87@gmail.com>
2024-08-27 11:19:20 -0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
 import HomePage from '../views/HomePage';
 import { GlobalProvider } from '../component/checkSomeThing';
 import SignUp1 from '../views/SignUp1';
@@ -7,24 +10,43 @@ import ViewBook2 from '../views/ViewBook2';
 import AsideCategory from '../component/AsideCategory';
 import CheckOut from '../views/Checkout';
 import Games from '../views/Games';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <HomePage/>,
+  },
+  {
+    path:"/viewbook2",
+    element:<ViewBook2 />,
+  },
+  {
+    path: "/SignUp1",
+    element:<SignUp1/>,
+  },
+  {
+    path:'/SignIn', 
+    element: <SignIn/>,
+  },
+  {
+    path:'/category', 
+    element:<AsideCategory/>, 
+  },
+  {
+    path:'/CheckOut', 
+    element:<CheckOut/>,
+  },
+  {
+    path:'/games', 
+    element:<Games/>,
+  }
+])
 const App = () => {
 
   return (
    <>
    <GlobalProvider>
-     <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/viewbook2" element={<ViewBook2 />}/>
-          <Route path="/SignUp1" element={<SignUp1/>}/>
-          <Route path='/SignIn' element={<SignIn/>}/>
-          <Route path='/category' element={<AsideCategory/>}/>
-          <Route path='/CheckOut' element={<CheckOut/>}/>
-          <Route path='/games' element={<Games/>}/>
-        </Routes>
-      </div>
-    </Router>
+     <RouterProvider router={router}/>
     </GlobalProvider>
    </>
   );
